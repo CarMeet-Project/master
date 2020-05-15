4243f387d3896c2b7a124bf6b3b75124efd4d645
markyyyy <markd.hartogh@Gmail.com>
2020-05-15 10:04:09 +0200

    footer/header
    de footer en header wat in het docu stond naar een appart document zetten zodat we het later maar 1x moeten verranderen

---
+++ views/footer.php
@@ -0,0 +1,5 @@
+<footer class="main-footer">
+            <div class="container main-footer-container">
+                <h3 class="brand-name">smoothroadcode</h3>
+            </div>
+        </footer>
---
+++ views/header.php
@@ -0,0 +1,11 @@
+<header class="main-header">
+            <nav class="nav main-nav">
+                <ul>
+                    <li><a href="index.php">HOME</a></li>
+                    <li><a href="store">STORE</a></li>
+                    <li><a href="about-us">ABOUT</a></li>
+                </ul>
+            </nav>
+            <h1 class="brand-name brand-name-large">carmeet</h1>
+            <img src="Images/mvm.png" alt="">
+        </header>
