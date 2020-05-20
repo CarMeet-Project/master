1e563d13acb9cf23372c741f8ee615405a4700df
BobNab <45999993+BobNab@users.noreply.github.com>
2020-05-20 13:44:25 +0200

    update

---
+++ build & test/login/V1/index.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once("Core/autoloader.php");
+
+$controller = new Controller();
+$controller->parser();
+
+?>
+
+<form action="./view/login/authenticate.php" method="post">
+        <label for="username">
+            <i class="fas fa-user"></i>
+        </label>
+        <input type="text" name="username" placeholder="Username" id="username" required>
+        <label for="password">
+            <i class="fas fa-lock"></i>
+        </label>
+        <input type="password" name="password" placeholder="Password" id="password" required>
+        <input type="submit" value="Login">
+    </form>
+</div>
