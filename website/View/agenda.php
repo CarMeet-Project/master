259648c297928939a2e9c1c6c843a6f398384120
StrandedHobo <haitambacha0903@hotmail.com>
2020-05-26 21:19:14 +0200

    Dev agenda hide

--- website/View/agenda.php
+++ website/View/agenda.php
@@ -2,20 +2,20 @@
 
 <?php
    $CalendarMonthOne = new Calendar(5,2020);
-   $CalendarMonthTwo = new Calendar(6,2020);
-   $CalendarMonthThree = new Calendar(7,2020);
-   $CalendarMonthFour = new Calendar(8,2020);
-   $CalendarMonthFive = new Calendar(9,2020);
-   $CalendarMonthSix = new Calendar(10,2020);
+   //$CalendarMonthTwo = new Calendar(6,2020);
+ //  $CalendarMonthThree = new Calendar(7,2020);
+  // $CalendarMonthFour = new Calendar(8,2020);
+  // $CalendarMonthFive = new Calendar(9,2020);
+ // $CalendarMonthSix = new Calendar(10,2020);
 ?>
 <div class="Calendars">
 <?php
    $CalendarMonthOne->show();
-   $CalendarMonthTwo->show();
-   $CalendarMonthThree->show();
-   $CalendarMonthFour->show();
-   $CalendarMonthFive->show();
-   $CalendarMonthSix->show();
+  // $CalendarMonthTwo->show();
+ //  $CalendarMonthThree->show();
+  // $CalendarMonthFour->show();
+  // $CalendarMonthFive->show();
+  // $CalendarMonthSix->show();
  ?>
  </div>
 <?php include("view/standard_content/footer.php"); ?>
