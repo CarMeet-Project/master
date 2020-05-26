f8feaf4d9c55b292ee0b024fe3c20cf7dbbbb71c
markyyyy <45282798+markyyyydh@users.noreply.github.com>
2020-05-26 14:45:36 +0200

    update about us

--- website/View/aboutUs.php
+++ website/View/aboutUs.php
@@ -1,3 +1,41 @@
 <?php include("View/standard_content/header.php"); ?>
 Dit is de about us page.
+
+
+<div> 
+    <h1> Wie zijn wij?</h1> 
+
+    Wij zijn NAME_HERE een groep vrienden die houden van auto's en het idee kregen om carmeets makkelijker beschikbaar te maken voor iedereen.<br>
+    en zijn wij op het idee gekomen om een website temaken waar je makkelijk en overzichtelijk een auto meet kan toevoegen als organizator en als gewoon lid makkelijk kan aanmelden.<br>
+    om te kijken of mensen ons idee handig en zouden willen gebruiken zijn wij naar meetings automeetings gegaan en hebben we rondgevraagd of mensen hier interesse in hadden.<br>
+    uit dit onderzoek is er gebleken dat hier best wel een interesse in is en zijn we verder gegaan met ons idee.<br>
+</div>
+
+
+<div> 
+    <h1> wat is een auto meeting</h1> 
+
+    een automeeting is eigenlijk heel simpel de naam beschrijft het eigenlijk al helemaal het is een groep mensen die samenkomt om te genieten van auto's.<br>
+    zo kunnen we ook nog andere dingen bij een carmeet gegooit worden zoals een 2step battle, autokeuringen etc.<br>
+    2step is eigenlijk een systeem waarmee je de ontbranding van de benzine in de cylinders kancontroleren en waar meestal 2 stages zijn 1 rond 3500 rpm en een rond het rpm limiet van de auto.<br>
+    de eerste laat de benzine later buiten de cylinders exploderen waardoor bij een auto met 1 of meer turbos de turbo ventilator sneller gaat ronddraaien en je meer lugt de moter in krijgt en de explosie buiten het block een luide knal geeft.<br>
+</div>
+
+<div> 
+    <h1> andere evenementen</h1> 
+
+    in de autowereld zijn meetings niet de enoge zo heb je ook nog rallies, shows, offroaden, races, dragraces, f1, en nog een hele hoop andere.
+
+</div>
+
+<div> 
+    <h1> cultuur</h1> 
+
+    er zijn net zo als overal veel verschillende culturen waaronder import, muscle etc.<br>
+    elke cultuur heeft weer andere soort dingen die ervoor zijn zoals bij import zie je vaak heel veel tuner auto's. <br>
+    en bij muscle is het vaak weer grote amerikaanse motoren.<br>
+    zo zijn er heel veel culturen en kan je zo ook kijken wat het best bij jou past omdat er zoveel verschil in zit en is er voor bijna iedereen wel wat.<br>
+    
+</div>
+
 <?php include("view/standard_content/footer.php"); ?>
