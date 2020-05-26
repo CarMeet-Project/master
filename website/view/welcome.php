ba0615dd77ec37ae578eaa7a495ef20fa652337b
StrandedHobo <haitambacha0903@hotmail.com>
2020-05-26 23:21:40 +0200

    Border voor falco

--- website/view/welcome.php
+++ website/view/welcome.php
@@ -1,7 +1,9 @@
 <?php include("View/standard_content/header.php"); ?>
 <div class="container">
         <div class="main">
-            Welcome to my Site<br>
+            <div class="bigWelcome">
+                <p>Welkom bij carmeetup</p>
+            </div>
         </div>
 </div>
 <?php include("View/standard_content/footer.php"); ?>
