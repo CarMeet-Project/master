989c3dfd112adb752bba2f97ebe102e61164fae6
StrandedHobo <haitambacha0903@hotmail.com>
2020-06-03 12:20:35 +0200

    yeet

--- website/standard_content/footer.php
+++ website/standard_content/footer.php
@@ -1,14 +1,5 @@
 <!DOCTYPE html>
 <html lang="en" >
-<head>
-  <meta charset="UTF-8">
-  <title>Footer</title>
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/><link rel="stylesheet" href="../assets/footer.css">
-
-</head>
 <body>
 <!-- Site footer -->
 <footer class="site-footer">
@@ -32,13 +23,13 @@
 
         <!-- if loggedin -->
         <?php if ((isset($_SESSION['loggedin'])) && ($_SESSION["loggedin"] == "TRUE")) { ?>
-          
+
         <li><a href="./index.php?action=logout">Uitloggen</a></li>
 
         <?php } else { ?>
-        
+
         <li><a href="./index.php?action=login">Inloggen</a></li>
-        
+
         <?php } ?>
         </ul>
       </div>
