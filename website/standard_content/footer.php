76d488a5c5d22f6f554a854770a2994a049c1c50
BobNab <45999993+BobNab@users.noreply.github.com>
2020-06-03 12:59:48 +0200

    set right? i gues

--- website/standard_content/footer.php
+++ website/standard_content/footer.php
@@ -3,10 +3,11 @@
 <head>
   <meta charset="UTF-8">
   <title>Footer</title>
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/><link rel="stylesheet" href="../assets/footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <link rel="stylesheet" href="./assets/style.css"/>
 
 </head>
 <body>
@@ -25,21 +26,18 @@
       <div class="col-xs-6 col-md-3">
         <h6>Quick Links</h6>
         <ul class="footer-links">
-        <li><a href="./index.php?action=welcome">Welcome</a></li>
-        <li><a href="./index.php?action=agenda">Agenda</a></li>
-        <li><a href="./index.php?action=aboutUs">About Us</a></li>
-        <li><a href="./index.php?action=contact">Contact</a></li>
+          <li><a href="./index.php?action=welcome">Welcome</a></li>
+          <li><a href="./index.php?action=agenda">Agenda</a></li>
+          <li><a href="./index.php?action=aboutUs">About Us</a></li>
+          <li><a href="./index.php?action=contact">Contact</a></li>
+  
+          <!-- if loggedin -->
+          <?php if ((isset($_SESSION['loggedin'])) && ($_SESSION["loggedin"] == "TRUE")) { ?>
+          <li><a href="./index.php?action=logout">Uitloggen</a></li>
+          <?php } else { ?>
+          <li><a href="./index.php?action=login">Inloggen</a></li>
+          <?php } ?>
 
-        <!-- if loggedin -->
-        <?php if ((isset($_SESSION['loggedin'])) && ($_SESSION["loggedin"] == "TRUE")) { ?>
-
-        <li><a href="./index.php?action=logout">Uitloggen</a></li>
-
-        <?php } else { ?>
-
-        <li><a href="./index.php?action=login">Inloggen</a></li>
-
-        <?php } ?>
         </ul>
       </div>
       <div class="col-md-8 col-sm-6 col-xs-12">
