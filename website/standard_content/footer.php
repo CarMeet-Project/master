3f513b1cb4d45420089e0231122cd2d922965220
BobNab <45999993+BobNab@users.noreply.github.com>
2020-06-09 13:38:42 +0200

    styling the css code

--- website/standard_content/footer.php
+++ website/standard_content/footer.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="en" >
-<head>
-  <meta charset="UTF-8">
-  <title>Footer</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-    <link rel="stylesheet" href="./assets/style.css"/>
-
-</head>
-<body>
 <!-- Site footer -->
 <footer class="site-footer">
   <div class="container">
--- website/standard_content/header.php
+++ website/standard_content/header.php
@@ -6,6 +6,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
     <link rel="stylesheet" href="./assets/style.css"/>
+    <link rel="stylesheet" href="./assets/header.css"/>
   	<title>MVC</title>
 </head>
 <body>
