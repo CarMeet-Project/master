bb0d494698904e151f0a0f4cb74c19879ee8a31c
markyyyy <45282798+markyyyydh@users.noreply.github.com>
2020-06-16 22:49:04 +0200

    homepage grid try
    17/6/2020 verder met groep

--- website/content/page/welcome.php
+++ website/content/page/welcome.php
@@ -1,7 +1,52 @@
-<div class="container">
-        <div class="main">
-            <div class="bigWelcome">
-                <p>Welkom bij carmeetup</p>
-            </div>
-        </div>
+
+
+  <div>
+  <div class="col-12 col-s-12 footer">
+      Lorem ipsum dolor sit amet.
+  </div>
+  </div>
+
+  <div>
+    <div class="col-12 col-s-12 footer">
+      banner
+    </div>
+  </div>
+
+  <div class="col-4 col-s-4 footer">
+    <div class="aside">
+        <h2>Lorem</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <h2>Lorem.</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <h2>Lorem.</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+  </div>
+
+  <div class="col-4 col-s-4 footer">
+    <div class="aside">
+        <h2>Lorem</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <h2>Lorem.</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <h2>Lorem.</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+  </div>
+
+  <div class="col-4 col-s-4 footer">
+    <div class="aside">
+        <h2>Lorem</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <h2>Lorem.</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <h2>Lorem.</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+  </div>
+
+<div>
+    <div class="col-12 col-s-12 footer">
+        Lorem ipsum dolor sit amet.
+    </div>
 </div>
