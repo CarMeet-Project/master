2d043ac3eb79c8428b5807e72717594cad41c24e
markyyyy <45282798+markyyyydh@users.noreply.github.com>
2020-06-16 22:50:40 +0200

    footer terug in plaats van grid style

--- website/standard_content/header.php
+++ website/standard_content/header.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
     <link rel="stylesheet" href="./assets/style.css"/>
     <link rel="stylesheet" href="./assets/header.css"/>
-    <link rel="stylesheet" href="./assets/grid.css">
+    <!-- <link rel="stylesheet" href="./assets/grid.css"> -->
   	<title>Car meet</title>
 </head>
 <body>
