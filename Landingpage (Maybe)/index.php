ee849f892402780f9df6c02f737765e05acee6fa
Harisan02 <531493@edu.rocmn.nl>
2020-06-17 01:53:20 +0200

    maybe

---
+++ Landingpage (Maybe)/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="./assets/landingpage.css"/>
+    </head>
+
+<?php
+
+require("header.php");
+?>
+
+<body>
+    <header id="showcase">
+        <h1>Welkom bij SmoothRoadCode!</h1>
+        <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore, nesciunt. Deserunt sapiente qui aspernatur illo.</p>
+        <a href="#" class="button">Over ons</a>
+    </header>
+    <section id="section-a">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nostrum pariatur excepturi consequuntur veniam exercitationem sed accusantium
+        corporis temporibus quia animi quod,
+        beatae eos dolore odio maiores alias expedita sapiente ipsa possimus repellendus mollitia! Magnam officia itaque laborum quasi distinctio.</p>
+    </section>
+    <section id="section-b">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nostrum pariatur excepturi consequuntur veniam exercitationem sed accusantium
+        corporis temporibus quia animi quod,
+        beatae eos dolore odio maiores alias expedita sapiente ipsa possimus repellendus mollitia! Magnam officia itaque laborum quasi distinctio.</p>
+    </section>
+    <section id="section-c">
+        <div class="box1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius similique magnam rerum eum nisi quo numquam voluptatem, soluta maxime qui!
+        </div>
+        <div class="box2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius similique magnam rerum eum nisi quo numquam voluptatem, soluta maxime qui!
+        </div>
+        <div class="box3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius similique magnam rerum eum nisi quo numquam voluptatem, soluta maxime qui!
+        </div>
