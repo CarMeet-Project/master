b7f6141adc7b1e2046d3271a73f477d4a5e0ac64
BobNab <45999993+BobNab@users.noreply.github.com>
2020-06-17 09:22:48 +0200

    push

--- website/standard_content/footer.php
+++ website/standard_content/footer.php
@@ -1,47 +1,57 @@
-<!-- Site footer -->
-<footer class="site-footer">
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-12 col-md-6">
-        <h6>About</h6>
-        <p class="text-justify">SmoothRoadCode <i> <br>
-         SmoothRoadCode is gespecialiseerd in het creeren van websites. <br>
-         Ons bedrijf is gevestigd in Nieuwegein & bestaat sinds 2020. <br>
-         Dit project is bevestigd in verband met passie en liefde tegenover auto`s. </p>
-      </div>
+<footer>
+        <div class="main-content">
+            <div class="left box">
+            <h2> Over ons</h2>
+            <div class="content">
+                <p> SmoothRoadCode is gespecialiseerd in het creeren van websites.
+                    Ons bedrijf is gevestigd in Nieuwegein & bestaat sinds 2020.
+                    Dit project is bevestigd in verband met passie en liefde tegenover auto`s.</p>
+                <div class="social">
+                    <a href="https://www.facebook.com/hoi.smoothroadcode.1"><span class="fab fa-facebook-f"></span></a>
+                    <a href="https://twitter.com/road_smooth"><span class="fab fa-twitter"></span></a>
+                </div>
+            </div>
+            </div>
+            <div class="center box">
+                <h2>Adres</h2>
+                <div class="content">
+                    <div class="place">
+                        <span class="fas fa-map-marker-alt"></span>
+                        <span class="text">Nieuwegein, Nederland</span>
+                    </div>
+                    <br>
+                    <div class="phone">
+                        <span class="fas fa-phone-alt"><a href="tel:+31612345678"></a></span>
+                        <span class="text">06-12345678</span>
+                    </div>
+                    <br>
+                    <div class="email">
+                        <span class="fas fa-envelope"></span>
+                        <span class="text">carmeetproject@gmail.com</span>
+                    </div>
+                </div>
+            </div>
+            <div class="right box">
+                <h2>Contact ons!</h2>
+                <div class="content">
+                    <form action="#">
+                        <div class="email">
+                            <div class="text"> E-mailadres *</div>
+                            <input type="email" required>
+                        </div>
+                        <div class="msg">
+                            <div class="text"> Uw bericht *</div>
+                                <textarea rows="2" cols="25" required></textarea>
+                            </div>
+                        <div class="btn">
+                            <button type="submit">Verstuur</button>                        
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </footer>
+ 
 
-      <div class="col-xs-6 col-md-3">
-        <h6>Quick Links</h6>
-        <ul class="footer-links">
-          <li><a href="./index.php?action=welcome">Home</a></li>
-          <li><a href="./index.php?action=agenda">Agenda</a></li>
-          <li><a href="./index.php?action=contact">Contact</a></li>
-          <li><a href="./index.php?action=aboutUs">OverOns</a></li>
-          <?php if ((isset($_SESSION['loggedin'])) && ($_SESSION["loggedin"] == "TRUE")) { ?>
-          <li><a href="./index.php?action=logout">Uitloggen</a></li>
-          <?php } else { ?>
-          <li><a href="./index.php?action=login">Inloggen</a></li>
-          <?php } ?>
-        </ul>
-      </div>
-    </div>
-    <hr>
-  </div>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-8 col-sm-6 col-xs-12">
-        <p class="copyright-text">Copyright &copy; 2020 All Rights Reserved by 
-     <a href="#">SmoothRoadCode</a>.
-        </p>
-        <div class="col-md-4 col-sm-6 col-xs-12">
-            <ul class="social-icons">
-              <li><a class="facebook" href="https://www.facebook.com/hoi.smoothroadcode.1" target="_blank"><i class="fa fa-facebook"></i></a></li>
-              <li><a class="twitter" href="https://twitter.com/road_smooth" target="_blank"><i class="fa fa-twitter"></i></a></li>
-            </ul>
-      </div>
-    </div>
-  </div>
-</footer>
-  
 </body>
 </html>
