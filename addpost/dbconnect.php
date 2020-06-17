dc34302e40d6aa465c9da16f0523cfae708937a5
StrandedHobo <haitambacha0903@hotmail.com>
2020-06-17 09:31:50 +0200

    upload

--- addpost/dbconnect.php
+++ addpost/dbconnect.php
@@ -2,11 +2,11 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$db = "car_meet";
+$db = "mypeople";
 
 $conn = new mysqli($servername, $username, $password, $db);
 
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
-} 
+}
  ?>
--- addpost/makePost.php
+++ addpost/makePost.php
@@ -7,15 +7,14 @@
     $discription = $_POST['discription'];
     $date = $_POST['date'];
 
-    echo $userId . $header . $discription . $date;
+    $sql = "INSERT INTO meetup (userid, header, description, date)
+    VALUES ('$userId', '$header', '$discription', '$date')";
 
-    $sql = "INSERT INTO posts (userid, header, discription, date) VALUES ($userId, $header, $discription, $date)";
-
-    if (mysqli_query($conn, $sql)) {
-        echo "uw post is toegevoegd.";
+    if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+      echo "Error: " . $sql . "<br>" . $conn->error;
     }
 
-$conn->close();
- ?>
+    $conn->close();
+    ?>
