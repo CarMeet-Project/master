af2b1aa3644c1762de5e8f6624fa96d69bb9806e
BobNab <45999993+BobNab@users.noreply.github.com>
2020-06-17 11:45:14 +0200

    update

--- website/content/page/welcome.php
+++ website/content/page/welcome.php
@@ -1,32 +1,30 @@
-
-
-  <div>
-  <div class="col-12 col-s-12 footer">
-      <img class="mainpic"src="./assets/img/carmeetup.jpg" >
+<div>
+  <div class="col-12 col-s-12 color">
+    <img class="mainpic"src="./assets/img/carmeetup.jpg" >
   </div>
-  </div>
+</div>
 
 
-  <div class="col-4 col-s-4 footer">
+<div class="col-4 col-s-4 color">
   <div class="aside">
-      <p><img class="imgclass_homegrid" src="./assets/img/car.jpg" ></p>
-    </div>
+    <p><img class="imgclass_homegrid" src="./assets/img/car.jpg" ></p>
   </div>
+</div>
 
-  <div class="col-4 col-s-4 footer">
-    <div class="aside">
-      <p><img class="imgclass_homegrid" src="./assets/img/carmeet.jpg" ></p>
-    </div>
-  </div>
-
-  <div class="col-4 col-s-4 footer">
+<div class="col-4 col-s-4 color">
   <div class="aside">
-      <p><img class="imgclass_homegrid" src="./assets/img/car.jpg" ></p>
-    </div>
+    <p><img class="imgclass_homegrid" src="./assets/img/carmeet.jpg" ></p>
   </div>
+</div>
+
+<div class="col-4 col-s-4 color">
+  <div class="aside">
+    <p><img class="imgclass_homegrid" src="./assets/img/car.jpg" ></p>
+  </div>
+</div>
 
 <div>
-    <div class="col-12 col-s-12 footer">
-        Lorem ipsum dolor sit amet.
-    </div>
+  <div class="col-12 col-s-12 color">
+    <p>Lorem ipsum dolor sit amet.</p>
+  </div>
 </div>
