2c2c6ad8c509cbdd58bb9b43f28803425f3d9291
markyyyy <45282798+markyyyydh@users.noreply.github.com>
2020-06-22 11:57:58 +0200

    mijn idee van welkome page

--- website/content/page/welcome.php
+++ website/content/page/welcome.php
@@ -8,18 +8,24 @@
 <div class="column-4 column-s-4 color">
   <div class="aside">
     <p><img class="imgclass_homegrid" src="./assets/img/car.jpg" ></p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab exercitationem ea quaerat, repudiandae doloremque ipsam alias suscipit. Eius illo repudiandae molestias voluptas explicabo quae, tempore qui ipsa! Possimus, asperiores voluptas.</p>
+    <a href="https://www.youtube.com/">de link</a>
   </div>
 </div>
 
 <div class="column-4 column-s-4 color">
   <div class="aside">
     <p><img class="imgclass_homegrid" src="./assets/img/carmeet.jpg" ></p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab exercitationem ea quaerat, repudiandae doloremque ipsam alias suscipit. Eius illo repudiandae molestias voluptas explicabo quae, tempore qui ipsa! Possimus, asperiores voluptas.</p>
+    <a href="https://www.youtube.com/">de link</a>
   </div>
 </div>
 
 <div class="column-4 column-s-4 color">
   <div class="aside">
     <p><img class="imgclass_homegrid" src="./assets/img/car.jpg" ></p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab exercitationem ea quaerat, repudiandae doloremque ipsam alias suscipit. Eius illo repudiandae molestias voluptas explicabo quae, tempore qui ipsa! Possimus, asperiores voluptas.</p>
+    <a href="https://www.youtube.com/">de link</a>
   </div>
 </div>
 
