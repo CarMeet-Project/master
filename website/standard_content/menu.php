d26329180d7c796c1f6048c45ea9974bcdaeb93e
unknown <525451@edu.rocmn.nl>
2020-06-23 13:59:23 +0200

    header aanpassing

--- website/standard_content/menu.php
+++ website/standard_content/menu.php
@@ -1,5 +1,8 @@
+ <div class="menuback">
 <nav class="global">
-    <ul>
+  
+  
+   <ul>
         <li><a href="./index.php?action=welcome">Home</a></li>
         <li><a href="./index.php?action=agenda">Agenda</a></li>
         <li><a href="./index.php?action=info">Info</a></li>
@@ -16,7 +19,7 @@
                 <a href="./index.php?action=add_product">Toevoegen</a>
                 <a href="./index.php?action=updatelist_product">Wijzigen</a>
                 <a href="./index.php?action=deletelist_product">Deactiveren</a>
-                <a href="./index.php?action=deletedlist_product">Verwijderde</a>
+                <a href="./index.php?action=deletedlist_product">Verwijderen</a>
             </div>
         </li>
         <li><a href="./index.php?action=cms">CMS</a></li>
@@ -37,4 +40,8 @@
         <?php } ?>
 
     </ul>
+	
+	
 </nav>
+
+</div>
