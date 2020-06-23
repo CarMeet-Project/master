a58329461cece49af19b995449b67d2568400fea
StrandedHobo <haitambacha0903@hotmail.com>
2020-06-24 00:00:17 +0200

    Revert
    aangezie bob verder ging met agenda zijn al mijn veranderingen van de laatste 2 dagen nutteloos

--- website/content/page/agenda.php
+++ website/content/page/agenda.php
@@ -5,27 +5,11 @@
         <title>Meetups</title>
     </head>
     <body>
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <?php if ((isset($_SESSION['loggedin'])) && ($_SESSION['loggedin'] == "TRUE")) { ?>
-        <div class="floatR">
-            <a class="btn btn-outline-success" href="./index.php?action=new_meet">Meet toevoegen</a>
-            <a class="btn btn-outline-danger" href="./index.php?action=delete_meet">Meet verwijderen</a>
-        </div>
-    <?php } ?>
-        <?php include("./content/meetup/show-meet.php");?>
-=======
-=======
->>>>>>> parent of 5ba7f97... Merge branch 'master' of https://github.com/CarMeet-Project/master
     <?php if ((isset($_SESSION['admin-loggedin'])) && ($_SESSION['admin-loggedin'] == "TRUE")) { ?>
         <div class="floatR">
         <a class="btn btn-outline-success" href="./index.php?action=new_meet">Meet toevoegen</a>
     </div>
     <?php } ?>
         <?php include("./content/meetup/show-meet.php") ?>
-<<<<<<< HEAD
->>>>>>> parent of 5ba7f97... Merge branch 'master' of https://github.com/CarMeet-Project/master
-=======
->>>>>>> parent of 5ba7f97... Merge branch 'master' of https://github.com/CarMeet-Project/master
     </body>
 </html>
