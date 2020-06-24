9722f0d4b1f0e920137f70e5110d712a7c291c55
BobNab <45999993+BobNab@users.noreply.github.com>
2020-06-24 12:43:06 +0200

    deactivate infopage

--- website/content/alerts/delete.php
+++ website/content/alerts/delete.php
@@ -15,7 +15,7 @@ if($succesFull == "FALSE") { ?>
   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
     <span aria-hidden="true">&times;</span>
   </button>
-  <strong>Succesvol</strong> Uw gekozen meetup is verwijderd
+  <strong>Succesvol</strong> gedeactiveerd
 </div>
 
 <?php } ?>
