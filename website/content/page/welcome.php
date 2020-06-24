afdf619d05f4190659e426fbe851e5b1f9112708
unknown <525451@edu.rocmn.nl>
2020-06-24 12:43:44 +0200

    menu changes

--- website/content/page/welcome.php
+++ website/content/page/welcome.php
@@ -29,7 +29,7 @@
 </div>
 
 
-<div id="carouselExampleIndicators" class="carousel slide column-12 column-s-12 color ">" data-ride="carousel">
+<div id="carouselExampleIndicators" class="carousel slide column-12 column-s-12 color ">
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
